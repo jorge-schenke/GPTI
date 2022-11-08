7bab7ab476f639fa85e7701b6031b511b7d16225
Jorge Schenke <jorge@examedi.com>
2022-11-08 12:36:18 -0300

    Cruz Verde + Ahumada listas :fire:

--- pages/api/[query].js
+++ pages/api/[query].js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import cheerio from 'cheerio';
+import puppeteer from 'puppeteer';
 
 export default async function handler(req, res) {
     const query = req.query.query;
@@ -12,7 +13,6 @@ export default async function handler(req, res) {
     const dataList = [];
     const getSearch = async (querySearch, responseList) => {
         const drSimiURL = `https://www.drsimi.cl/${query}?_q=${query}&map=ft`;
-        console.log("Dr Simi URL: ", drSimiURL);
         const cruzVerdeURL = `https://www.cruzverde.cl/search?query=${query}`;
         const AhumadaURL = `https://www.farmaciasahumada.cl/catalogsearch/result/?q=${query}`;
         const salcobrandURL = `https://salcobrand.cl/search_result?query=${query}`;
@@ -34,18 +34,77 @@ export default async function handler(req, res) {
                 source
             });
         });
-        console.log("Dr Simi List: ", drSimiList);
         //add to dataList
-        responseList.push(drSimiList);
+        responseList.push.apply(responseList, drSimiList);
         //Cruz Verde
-        const cruzVerdeData = await getRawData(cruzVerdeURL);
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(cruzVerdeURL);
+        await page.waitForSelector('ml-card-product');
+        const cruzVerdeData = await page.content();
+        const $CV = cheerio.load(cruzVerdeData);
+        const cruzVerdeList = [];
+        $CV('ml-card-product').each((i, el) => {
+            const title = $CV(el).find('span.ng-star-inserted').text().trim();
+            const price = parseInt($CV(el).find('span.text-orange').text().replace("$", "").replace(".", ""));
+            const link = "https://www.cruzverde.cl" + $CV(el).find('a').attr('href');
+            const image = $CV(el).find('img.max-w-full').attr('src');
+            const source = "Cruz Verde";
+            cruzVerdeList.push({
+                title,
+                price,
+                link,
+                image,
+                source
+            });
+        });
+        //add to dataList
+        responseList.push.apply(responseList, cruzVerdeList);
         //Ahumada
         const AhumadaData = await getRawData(AhumadaURL);
+        const $AH = cheerio.load(AhumadaData);
+        const AhumadaList = [];
+        $AH('.product-item-info').each((i, el) => {
+            const title = $AH(el).find('.product-item-link').text().replace('\n', '').replace(/\s+/g, ' ').trim();
+            const price = parseInt($AH(el).find('.price').text().replace("$", "").replace(".", ""));
+            const link = $AH(el).find('.product-item-photo').attr('href');
+            const image = $AH(el).find('.product-image-photo').attr('src');
+            const source = "Ahumada";
+            AhumadaList.push({
+                title,
+                price,
+                link,
+                image,
+                source
+            });
+        });
+        //add to dataList
+        responseList.push.apply(responseList, AhumadaList);
         //Salcobrand
-        const salcobrandData = await getRawData(salcobrandURL);
+        // console.log("Salcobrand URL: ", salcobrandURL);
+        // const salcobrandData = await getRawData(salcobrandURL);
+        // const $SB = cheerio.load(salcobrandData);
+        // console.log("Salcobrand Data: ", salcobrandData);
+        // const salcobrandList = [];
+        // $SB('.product').each((i, el) => {
+        //     const title = $SB(el).find('.product-info').text().replace('\n', '').replace(/\s+/g, ' ').trim();
+        //     const price = parseInt($SB(el).find('.original-price').text().replace("Precio farmacia: $", "").replace(".", ""));
+        //     const link = $SB(el).find('a').attr('href');
+        //     const image = $SB(el).find('.img-responsive').attr('src');
+        //     const source = "Salcobrand";
+        //     salcobrandList.push({
+        //         title,
+        //         price,
+        //         link,
+        //         image,
+        //         source
+        //     });
+        // });
+        // console.log("Salcobrand List: ", salcobrandList);
     }
     // try{
     await getSearch(query, dataList);
+    console.log("Data List: ", dataList);
     //send detaList in response:
     res.status(200).json({ dataList });
     // } catch {
