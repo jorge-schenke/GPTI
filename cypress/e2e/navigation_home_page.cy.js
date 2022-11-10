913e1900cddd8fc4c1000645ecc23e9d311bdacb
MACARENA FILLOL <mafillol@uc.cl>
2022-11-10 01:55:12 -0300

    feature: add test navigation from home page

---
+++ cypress/e2e/navigation_home_page.cy.js
@@ -0,0 +1,49 @@
+describe('Navigation Home Page', () => {
+
+  beforeEach(() => {
+    //Give an alias to the request
+    cy.intercept({
+      method: 'GET',
+      url: '/_next/static/development/_devPagesManifest.json'
+    }).as('afterClick')
+  })
+
+  it('should navigate to about page from home', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+    // Click about in nav bar
+    cy.get('a[href="/about"]', {timeout: 30000}).contains('About').click()
+    // Wait for API response
+    cy.wait('@afterClick', { responseTimeout: 15000})
+    // The new url should include "/about"
+    cy.url({timeout: 15000}).should('include', '/about')
+    // The new page should contain an h2 with "¿Quienes Somos?"
+    cy.get('h2').contains('¿Quienes Somos?')
+  })
+
+  it('should stay in home page with home button', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+    // Click home in nav bar
+    cy.get('a[href="/"]', {timeout: 15000}).contains('Home').click()
+    // Wait for API response
+    cy.wait('@afterClick', { responseTimeout: 15000})
+    // The url should be index page
+    cy.url({timeout: 15000}).should('eq', 'http://localhost:3000/')
+    // The page should contain an h1 with "'¡Bienvenido a Ecomedi!'"
+    cy.get('h1[id="welcome"]').contains('¡Bienvenido a Ecomedi!')
+  })
+
+  it('should stay in home page with logo', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+    // Click logo in nav bar
+    cy.get('a[href="/"]', {timeout: 30000}).contains('Ecomedi ').click()
+    // Wait for API response
+    cy.wait('@afterClick', { responseTimeout: 15000})
+    // The url should be index page
+    cy.url({timeout: 15000}).should('eq', 'http://localhost:3000/')
+    // The page should contain an h1 with "'¡Bienvenido a Ecomedi!'"
+    cy.get('h1[id="welcome"]').contains('¡Bienvenido a Ecomedi!')
+  })
+})
