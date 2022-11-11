9ee85c81f63559a424c71aff2b3e958da9360b39
MACARENA FILLOL <mafillol@uc.cl>
2022-11-11 01:39:33 -0300

    feature: add component testing LoadingScreen

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -1,11 +1,11 @@
 export const LoadingScreen = () => {
     return (
-        <div className="loading-screen">
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
-            <div className="dot"></div>
+        <div className="loading-screen" data="loading">
+            <div className="dot" data="dot"></div>
+            <div className="dot" data="dot"></div>
+            <div className="dot" data="dot"></div>
+            <div className="dot" data="dot"></div>
+            <div className="dot" data="dot"></div>
         </div>
     );
 };
